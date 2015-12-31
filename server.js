81deaf33391799c0580bab907fc35f3e6e3deec1
CHINNU <phaniukesht@gmail.com>
2015-12-31 11:38:00 -0600

    changing path for running server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express =require('express');
 var server = express();
-server.use(express.static('/myapp'));
+server.use(express.static('/My3_angular'));
 var port = 10001;
 server.listen(port,function(){
 	console.log('server listening on port ' + port);
