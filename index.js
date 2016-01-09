293e372872ab723f592a8639fbc7a00c42cf19d6
CHINNU <phaniukesht@gmail.com>
2016-01-09 09:47:26 -0600

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.application = "My3/IIS employee desk";
+    $scope.application = "My3/IIS tech touch-Angular concepts";
     var day;
     switch (new Date().getDay()) {
     case 0:
@@ -27,7 +27,13 @@ app.controller('myCtrl', function($scope) {
         day = "Saturday";
         break;
 }
+
+if(day == "Friday"){
+    day = "Happy friday";
+} else if(day == "Saturday" || day == "Sunday") {
+    day = "Happy Weekend";
+}
     	$scope.idx = day;
-    
+        $scope.d = new Date();
 
 });
